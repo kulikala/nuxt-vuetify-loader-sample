73f5daca682165a981be207735b87eed754c2386
Kaz Namba <kaz.namba@gmail.com>
2019-11-06 16:03:24 +0900

    chore: update sample files

---
+++ components/app/welcome/MessageBoard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <slot />
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
